063124b7bcfccd9c86754ed6ab9796b5a239d858
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2025-11-25 01:40:10 +0530

    Word boxes added

--- src/WordleGame.java
+++ src/WordleGame.java
@@ -1,12 +1,28 @@
 import javax.swing.*;
+import java.awt.*;
+// import java.awt.event.*;
 
 class WordleGame extends JFrame {
+    int wordBoxX = 10;
+
     public WordleGame() {
         this.setTitle("Wordle Game");
         this.setSize(400, 600);
+        this.getContentPane().setBackground(new Color(55, 50, 50));
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
+
+        JPanel[] wordBox = new JPanel[5];
+        for (int i = 0; i < 5; i++) {
+            wordBox[i] = new JPanel();
+            wordBox[i].setBackground(new Color(30, 30, 30));
+            wordBox[i].setBounds(wordBoxX += 55, 10, 50, 50);
+            this.add(wordBox[i]);
+        }
+
+        this.revalidate();;
+        this.repaint();
         this.setVisible(true);
     }
 
