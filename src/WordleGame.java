7b0686a242c14d00e8d1ef61f87a41db2c699ac5
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2025-11-25 03:31:58 +0530

    PlayGame method + Words class added

--- src/WordleGame.java
+++ src/WordleGame.java
@@ -7,25 +7,19 @@ class WordleGame extends JFrame {
     int curBoxRow = 0;    // To navigate during typing
     int wordBoxX = 16;
     int wordBoxY = 10;
-
-    public WordleGame() {
-        this.setTitle("Wordle Game");
-        this.setSize(400, 600);
-        this.getContentPane().setBackground(new Color(55, 50, 50));
-        this.setResizable(false);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
-
+    String targetWord;
+    
+    void PlayGame() {
         JPanel[][] wordBox = new JPanel[6][5];
         JLabel[][] boxText = new JLabel[6][5];
-
+        
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 5; j++) {
                 wordBox[i][j] = new JPanel();
                 wordBox[i][j].setBackground(new Color(30, 30, 30));
                 wordBox[i][j].setBounds(wordBoxX += 52, wordBoxY, 50, 50);
                 this.add(wordBox[i][j]);
-    
+        
                 boxText[i][j] = new JLabel();
                 Font tempFont = boxText[i][j].getFont();
                 Font boxTextFont = tempFont.deriveFont(32f);
@@ -34,18 +28,26 @@ class WordleGame extends JFrame {
                 boxText[i][j].setText("");
                 wordBox[i][j].add(boxText[i][j]);
             }
-
+        
             wordBoxX = 16;
             wordBoxY += 52;
         }
-
+        
         this.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE && curBoxIndex > 0) {
                     boxText[curBoxRow][--curBoxIndex].setText("");
+                    return;
                 }
-
+        
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    if (false) {
+                        //
+                    }
+                    return;
+                }
+        
                 char tempChar = e.getKeyChar();
                 
                 if (curBoxIndex < 5 && Character.isAlphabetic(tempChar)) {
@@ -53,13 +55,24 @@ class WordleGame extends JFrame {
                 }
             }
         });
-
+        
         this.setFocusable(true);
         this.requestFocus();
-
+        
         this.revalidate();;
         this.repaint();
         this.setVisible(true);
+    }
+    
+    public WordleGame() {
+        this.setTitle("Wordle Game");
+        this.setSize(400, 600);
+        this.getContentPane().setBackground(new Color(55, 50, 50));
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+
+        PlayGame();
     }
 
     public static void main(String args[]) {
---
+++ src/Words.java
@@ -0,0 +1,6 @@
+public class Words {
+    String[][] wordsList = {
+        {"ABOUT", "ABOVE", "AFTER", "AGAIN", "ALONG", "APPLY", "ANGER", "AMONG", "APPLE", "ALONE", "AGENT", "AGREE", "ALERT", "ALLOW", "AWARE", "ADULT", "ALIVE", "ANGEL", "APART", "AHEAD"},
+        {"BELOW", "BRING", "BUILD", "BREAD", "BRAND", "BLOOD", "BEACH", "BREAK", "BEGIN", "BLIND", "BENCH", "BLOCK", "BOUND", "BROWN", "BOARD", "BRIEF", "BASIC", "BUYER", "BURNS", "BURST"}
+    };
+}
