85ea8f2ad863685dad6e909014c7b53689d2c5dc
Wolffang021 <shourjo.maitra.21.2004@gmail.com>
2025-11-28 02:11:44 +0530

    Words.java split into spearate files

--- script/WordLoader.py
+++ script/WordLoader.py
@@ -21,7 +21,7 @@ with open("C:/Users/Shourjo/Downloads/Wordle_Word_Dictionary/wordle-Ta.txt", "r"
     taCode.append("}")
     taCode.append(", ")
 
-with open("src/Words.java", "w") as javaFile:
+with open("src/temp.txt", "w") as javaFile:
     taCode.pop()
     javaFile.write("public class Words {\n\tpublic String[][] wordsList = {")
     javaFile.write("".join(laCode))
